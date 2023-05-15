e974b5d7252b37bc4beb8c79d832f5dd72dbd168
MerajCodes <113118915+merajCodes3440@users.noreply.github.com>
2023-05-15 15:14:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // complete the given function
 
 function palindrome(str){
-
+   let isPlindrom = true;
+    let i=0 ; let j=str.length-1;
+	while(i<=j){
+		if(str[i]!==str[j]){
+           isPlindrom=false;
+		     return;
+		}
+		i++;
+		j--;
+   }
+	console.log(isPlindrom? "true":"false");
 }
 module.exports = palindrome
