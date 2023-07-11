a4199f170fcbe39d1f7597f09d3caaccd0145cdb
MerajCodes <113118915+merajCodes3440@users.noreply.github.com>
2023-07-11 10:10:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function palindrome(str){
    let isPlindrome = true;
     let i=0 ; let j=str.length-1;
 	while(i<=j){
-		if(str[i].toLowerCase()!==str[j].toLowerCase()){
+		if(str[i].toLowerCase().trim()!==str[j].toLowerCase().trim()){
            isPlindrome =false;
 		     return;
 		}
